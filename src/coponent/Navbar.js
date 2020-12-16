ea447216fcc33caea55dc58866d8bfde8efe0ce1
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-12-17 03:53:17 +0900

    ナビゲーションヘッダーの作成

---
+++ src/coponent/Navbar.js
@@ -0,0 +1,19 @@
+import React,{ Component } from 'react';
+import { Link } from 'react-router-dom';
+import './css/nav.css';
+
+class Navbar extends Component {
+  render() {
+    return (
+      <header className="navbar">
+        <h1 className="ptitle">binoculars</h1>
+        <ul className="list">
+          <li><Link to='/'>HomePage</Link></li>
+          <li><Link to='/Maparea'>GoogleMap</Link></li>
+          <li><Link to='Csv'>金沢市の施設の一覧</Link></li>
+        </ul>
+      </header>
+    );
+  }
+}
+export default Navbar;
