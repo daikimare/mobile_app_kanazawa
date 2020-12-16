693fc2c9e206f9d07e715d3d70c66aa3c73e1bdd
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-12-17 03:55:52 +0900

    CSVファイルの内容を表示するコンポーネントの作成

---
+++ src/coponent/CSV.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/main.css';
+
+function Csv() {
+  return(
+    <div className="csv">
+      <h1 className="csv-app">CSVの表示</h1>
+    </div>
+  );
+}
+Csv()
+export default Csv;
