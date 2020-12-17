9d03019e7d99dbe0ef42b4af138c166ada77ff6f
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2020-12-18 04:36:37 +0900

    追加編集したファイルのルーティング作業

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './coponent/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // page component
 import Maparea from './coponent/Maparea';
-import Csv from './coponent/CSV';
+import Csv from './coponent/Csv';
 
 const Home = () =>{
   return(
@@ -20,8 +19,8 @@ function App() {
         <Navbar />
         <Switch>
           <Route exact path='/' component={Home} />
-          <Route path='/' component={Maparea} />
-          <Route path='/' component={Csv} />
+          <Route path='/Maparea' component={Maparea} />
+          <Route path='/Csv' component={Csv} />
         </Switch>
       </Router>
     </div>
