d610eed5716aab75a2a056e223967a4dc008fec0
daikimare <b1840702@planet.kanazawa-it.ac.jp>
2021-01-14 04:59:42 +0900

    オリジナルMarkerの作成(v1が文字表記のみのため見にくかった)

--- src/coponent/Marker.js
+++ src/coponent/Marker.js
@@ -1,8 +1,19 @@
-import { func } from 'prop-types';
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { MarkerStyle } from './MarkerStyle';
 
-function Marker(){
-  return(
-    <div></div>
-  );
+export default class Marker extends Component {
+  static propTypes = {
+    text: PropTypes.string
+  };
+
+  static defaultProps = {};
+  
+  render() {
+    return (
+      <div style = {MarkerStyle}>
+        {this.props.text}
+      </div>
+    )
+  }
 }
